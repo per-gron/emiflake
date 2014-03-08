ada99b75d2fde767377c980a529e7799c18aa1fa
Alex Gorbatchev <alex.gorbatchev@gmail.com>
2014-03-08 08:47:13 -0800

    Makes test class name consistent with definition.

--- test/emiflake_test.js
+++ test/emiflake_test.js
@@ -1,10 +1,10 @@
 var chai = require('chai');
-var emiflake = require('..');
+var EmiFlake = require('..');
 var expect = chai.expect;
 
-describe('emiflake', function() {
+describe('EmiFlake', function() {
   it('generates IDs', function(done) {
-    var f = new emiflake(new Buffer("mid"), 24, 16, 24, 20000);
+    var f = new EmiFlake(new Buffer("mid"), 24, 16, 24, 20000);
     f.generate(function(id) {
       expect(id.length).to.eql(8);
       done()
